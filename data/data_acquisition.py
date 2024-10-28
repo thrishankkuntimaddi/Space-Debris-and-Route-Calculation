ad59706919f313b92af8ba3d08bf46e9832e6e27
thrishankkuntimaddi <thrishankkuntimaddi@gmail.com>
2024-10-28 23:23:45 +0530

    Check

---
+++ data/data_acquisition.py
@@ -0,0 +1,1 @@
+purpose = "Collects and preprocesses TLE and other relevant datasets for the mission’s orbital calculations."
---
+++ src/orbital_dynamics.py
@@ -0,0 +1,9 @@
+purpose = "Computes and returns the orbital position and velocity for a given timestamp."
+
+'''
+1. Load Preprocessed Dataset
+2. Calculate Orbital Mechanism
+   - Predict and extract position and velocity for specified timestamp.
+3. Output Orbital Data
+   - Return position and velocity for given timestamp.
+'''
