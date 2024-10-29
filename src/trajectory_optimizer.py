b6d92ce2fc76d2e3530ab46d0b618f9344f18e37
thrishankkuntimaddi <thrishankkuntimaddi@gmail.com>
2024-10-29 10:36:50 +0530

    Restructure

---
+++ src/trajectory_optimizer.py
@@ -0,0 +1,10 @@
+purpose = "Optimizes the rocket's trajectory by collision Detection."
+
+'''
+1. Receive Input Data
+   - Use `orbital mechanism`, `rocket description`, `timestamp`, and `orbit distance`.
+2. Evaluate Route
+   - Calculate possible route for the rocket.
+   - Optimize route if needed.
+3. Return Optimized Route
+'''
